e188d9e3c891b54b114674e8be9f8c8f98928800
chauhanchetna97 <42907579+chauhanchetna97@users.noreply.github.com>
2018-09-02 15:19:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,53 +16,37 @@ int main()
         else
         {
              for(int i=3;i<=n;i++)
-            {
-            
-            
+              {
                 r=p+q;
                 p=q;
                 q=r;
-        
-            }
-    
-         cout<<r;
+              }
+             cout<<r;
         }
     }    
         else if((n%2)==0)
         {
-           
              n=n/2;
-              int p=1,count=0;
+             int p=1,count=0;
              if(n==1)
              cout<<n+1;
-             
-           
-            else
+             else
             {   
                int y =n*4;
-               
                for(int i=3;i<y;i++)
                 {count=0;
                     for(int j=2;j<(i/2)+1;j++)
                       {
                           if(i%j==0)
                            ++count;
-                    
-                       }
+                      }
                     if(count==0)
-                    {
-                    p++;
-                    
-                   
-                    if(p==(n))
-                    cout<<i;}
-                    
+                     {
+                       p++;
+                       if(p==(n))
+                       cout<<i;}
+                     }
                 }
             }
-        }
-        
-    
-   
-   
    return 0;
 }
