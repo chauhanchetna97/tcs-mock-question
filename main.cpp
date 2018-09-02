02cfea818f8e4fbba121e44827ed55196a7e4010
chauhanchetna97 <42907579+chauhanchetna97@users.noreply.github.com>
2018-09-02 15:46:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,106 @@
 #include <iostream>
 
+
+
 using namespace std;
 
+
+
 int main()
+
 {  
+
     int n;
+
     cin>>n;
+
     if((n%2)!=0)
+
     {   
+
         n=(n/2)+1;
-        int p=1,q=1,r=0;
+
+        int r=0, p=1,q=1;
+
         if((n==1)||(n==2))
-        {cout<<p;}
+
+        cout<<p;
+
         
+
         else
-        {
+
+         {
+
              for(int i=3;i<=n;i++)
+
               {
+
                 r=p+q;
+
                 p=q;
+
                 q=r;
+
               }
+
              cout<<r;
-        }
+
+          }
+
     }    
+
         else if((n%2)==0)
+
         {
+
              n=n/2;
+
              int p=1,count=0;
+
              if(n==1)
+
              cout<<n+1;
+
              else
+
             {   
+
                int y =n*4;
+
                for(int i=3;i<y;i++)
+
                 {count=0;
+
                     for(int j=2;j<(i/2)+1;j++)
+
                       {
+
                           if(i%j==0)
+
                            ++count;
+
                       }
+
                     if(count==0)
+
                      {
+
                        p++;
+
                        if(p==(n))
-                       cout<<i;}
+
+                       cout<<i;
+                         
                      }
+
+                     
+
                 }
+
             }
+        }    
+
    return 0;
+
 }
